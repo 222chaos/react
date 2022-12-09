0d51f4802a876010d1e28da1382dcab551b241f5
Chaos <zchaos2@foxmail.com>
2022-12-10 00:57:24 +0800

    增加了challenge

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -12,6 +12,16 @@ function App() {
       rating: 4.8,
       reviews: 20,
     },
+    challenges: [
+      { name: "将切好的土豆加入一锅浓盐水中。", prepared: false },
+      { name: "把锅烧开。", prepared: false },
+      {
+        name: "将土豆煮至叉子可以轻松戳入的程度，大约 15-20 分钟。",
+        prepared: false,
+      },
+      { name: "将土豆的水沥干。", prepared: true },
+      { name: "把土豆再倒回锅里。", prepared: true },
+    ],
     ingredients: [
       { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
       { name: "4 Tbsp butter", prepared: false },
@@ -26,6 +36,7 @@ function App() {
       {/* TODO: Add RecipeTitle component */}
       <RecipeTitle title={recipe.title} feedback={recipe.feedback} />
       {/* TODO: Add IngredientList component */}
+      <IngredientList ingredients={recipe.challenges} />
       <IngredientList ingredients={recipe.ingredients} />
     </article>
   );
