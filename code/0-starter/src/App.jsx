86bfced8ae59f16654ec4158bb78842cf30dc241
Chaos <zchaos2@foxmail.com>
2022-12-12 22:49:50 +0800

    将app.jsx的内容改为todolist

--- code/0-starter/src/App.jsx
+++ code/0-starter/src/App.jsx
@@ -1,13 +1,46 @@
-import './index.css'
-import React from 'react';
-import RecipeTitle from './RecipeTitle'
-function App() {
+import React, { Component, Fragment } from "react";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "What need to be done",
+      list: [],
+    };
+  }
+
+  render() {
     return (
-        <article>
-            <h1>Recipe Manager</h1>
-            <RecipeTitle />
-        </article>
-    )
+      <Fragment>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+        </div>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </Fragment>
+    );
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
+    // console.log(e.target.value)
+  }
+
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: "",
+    });
+  }
 }
 
-export default App;
+export default TodoList;
