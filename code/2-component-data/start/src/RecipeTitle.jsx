0baa24c349c664d7252e22cb3b6eb1180d0f2091
Chaos <zchaos2@foxmail.com>
2022-12-25 22:54:53 +0800

    使用transition达到了渐变的效果

--- code/2-component-data/start/src/RecipeTitle.jsx
+++ code/2-component-data/start/src/RecipeTitle.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function TodoListItem(props) {
-  const [hover, setHover] = useState("black");
+  const [hover, setHover] = useState("white");
+  const [opstate, setOp] = useState(0);
+  useEffect(() => {
+    setOp(1);
+  });
+
   return (
     <li
       onMouseEnter={() => {
-        setHover("pink");
-      }}
-      onMouseLeave={() => {
         setHover("yellow");
       }}
+      onMouseLeave={() => {
+        setHover("pink");
+      }}
       style={{
+        transition: "all 1s",
+        opacity: opstate,
         background: hover,
-        color: "orange",
+        color: "blue",
       }}
     >
       {props.content}
