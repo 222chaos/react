9fb9dbf4be8d84d591d78c1129adef18b50f4760
Chaos <zchaos2@foxmail.com>
2022-12-28 00:08:05 +0800

    修改了todo的数据类型

--- code/2-component-data/start/src/RecipeTitle.jsx
+++ code/2-component-data/start/src/RecipeTitle.jsx
@@ -3,20 +3,26 @@ import React, { useState, useEffect } from 'react';
 function TodoListItem(props) {
   const [hover, setHover] = useState('white');
   const [opstate, setOp] = useState(0);
+  const [button, setB] = useState(0);
   useEffect(() => {
     setOp(1);
-  });
+  }, []);
 
   return (
     <>
       <li
         onMouseEnter={() => {
           setHover('yellow');
+          setB(1);
         }}
         onMouseLeave={() => {
           setHover('pink');
+          setB(0);
         }}
         style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
           transition: 'all 1s',
           opacity: opstate,
           background: hover,
@@ -24,6 +30,7 @@ function TodoListItem(props) {
         }}
       >
         {props.content}
+        {button === 1 ? <buttton>×</buttton> : null}
       </li>
       {props.splitLine ? <hr /> : null}
     </>
