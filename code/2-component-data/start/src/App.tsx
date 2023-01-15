638613cdd8dab687b851c5238399a98e6726b5db
Chaos <zchaos2@foxmail.com>
2023-01-16 01:03:45 +0800

    完成了简单分页

--- code/2-component-data/start/src/App.tsx
+++ code/2-component-data/start/src/App.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import TodoListItem from './RecipeTitle';
 
 function App() {
-  
   function change() {
     setTodo([...todo, state]);
     updateState('');
@@ -21,27 +20,32 @@ function App() {
   }
   const [todo, setTodo] = useState<Array<string>>([]);
   const [state, updateState] = useState<string>('0');
-  const [stateTitle,updateStateTitle] = useState<string>('im title')
-  const [stateDesc,updateStateDesc] = useState<string>('im desc')
-    let total=0
-    let pagesize=5
-  const [todo1, setTode] = useState(()=>{
-    const todoList=[]
-    for(let index=0;index<100;index++){
-      total++
+  const [stateTitle, updateStateTitle] = useState<string>('im title');
+  const [stateDesc, updateStateDesc] = useState<string>('im desc');
+  let total = 0;
+  let pagesize = 5;
+  const [clickPageNum, setClickPageNum] = useState<number>(1);
+  const [todo1, setTode] = useState(() => {
+    const todoList = [];
+    for (let index = 0; index < 100; index++) {
+      total++;
       todoList.push({
-        title:"title"+index,
-        desc:"desc"+index
-      })
+        title: 'title' + index,
+        desc: 'desc' + index,
+      });
     }
-    return todoList
+    return todoList;
   });
-  const [pageNum,setPageNum] =useState(0);
-    
-    
- 
+  /*const [pageNum, setPageNum] = useState(() => {
+    const pageList = [];
+    for (let index = 1; index < (total + pagesize - 1) / pagesize; index++) {
+      pageList.push(index);
+    }
+    return pageList;
+  });
+  */
+  const [pageNum, setPageNum] = useState<number>((total + pagesize - 1) / pagesize);
   return (
-    
     <article>
       <h1>Todo</h1>
       <input
@@ -56,54 +60,51 @@ function App() {
           }
         }}
       ></input>
-       <button
-         onClick={() => {
-          change()
-        
-      }}
+      <button
+        onClick={() => {
+          change();
+        }}
       >
         Orz
       </button>
-      <input 
-      value={stateTitle}
-      onChange={(e) => {
-        updateStateTitle(e.target.value);
-      }}
-      >
-      </input>
+      <input
+        value={stateTitle}
+        onChange={(e) => {
+          updateStateTitle(e.target.value);
+        }}
+      ></input>
       <button
-         onClick={() => {
-          if(stateDesc===""){
-              window.alert("There is no content in desc")
-              return
+        onClick={() => {
+          if (stateDesc === '') {
+            window.alert('There is no content in desc');
+            return;
           }
           setTode([...todo1, { title: stateTitle, desc: stateDesc }]);
           updateStateTitle('');
-          updateStateDesc('')
-        
-      }}
+          updateStateDesc('');
+        }}
       >
         Title
       </button>
       <input
-      value={stateDesc}
-      onChange={(e) => {
-        updateStateDesc(e.target.value);
-      }}
+        value={stateDesc}
+        onChange={(e) => {
+          updateStateDesc(e.target.value);
+        }}
       ></input>
-       <button
-         onClick={() => {
-          if(stateTitle===""){
-            window.alert("There is no content in title")
-            return
+      <button
+        onClick={() => {
+          if (stateTitle === '') {
+            window.alert('There is no content in title');
+            return;
           }
           setTode([...todo1, { title: stateTitle, desc: stateDesc }]);
-          updateStateDesc('')
-      }}
+          updateStateDesc('');
+        }}
       >
         Desc
       </button>
-     
+
       <ul>
         <a>{state}</a>
         {todo.map((item, index) => {
@@ -118,27 +119,39 @@ function App() {
             />
           );
         })}
-        {todo1.map((obj,index) => {
-          
-        
-          return (
-            <li>
-              <div>{obj.title}</div>
-              <div
-              style={{
-                color:"pink",
-                padding: 8,
-                fontSize:15
-              }}>{obj.desc}</div>
-              <hr></hr>
-            </li>
-          );
+        {todo1.map((obj, index) => {
+          if (index > (clickPageNum - 1) * pagesize - 1 && index < clickPageNum * pagesize)
+            return (
+              <li>
+                <div>{obj.title}</div>
+                <div
+                  style={{
+                    color: 'pink',
+                    padding: 8,
+                    fontSize: 15,
+                  }}
+                >
+                  {obj.desc}
+                </div>
+                <hr></hr>
+              </li>
+            );
         })}
 
-        {function a(){setPageNum((total+pagesize-1)/pagesize)
-    for (let index=0;index<pageNum;index++){
-      return(<button>{index}</button>)
-    }}}
+        {new Array(pageNum).fill(null).map((item, index) => {
+          return (
+            <button
+              style={{
+                background: index !== clickPageNum - 1 ? 'white' : 'red',
+              }}
+              onClick={() => {
+                setClickPageNum(item);
+              }}
+            >
+              {item}
+            </button>
+          );
+        })}
       </ul>
     </article>
   );
